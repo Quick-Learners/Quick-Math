805a1afd3decd91156d7aa621d9001bc0ab4d07a
Caleb McKinney <McKinney.Caleb@gmail.com>
2023-09-30 11:06:56 -0500

    updated bio and tested code

--- addapp.js
+++ addapp.js
@@ -5,26 +5,27 @@ let timer;
 let timeLeft = 60;
 
 function generateProblem() {
-    const num1 = Math.floor(Math.random() * 10) + 1; // provides a random number 1-10
-    const num2 = Math.floor(Math.random() * 10) + 1; // provides a random number 1-10
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
     document.getElementById("problem").textContent = `${num1} + ${num2} = ?`;
+ //   console.log(`Generated problem: ${num1} + ${num2}`);
 }
 
 function startTimer() {
     timer = setInterval(function() {
         timeLeft--;
         document.getElementById("timer").textContent = `Time left: ${timeLeft}s`;
+      //  console.log(`Time left: ${timeLeft}s`);
 
-// When the timer reaches zero
-if (timeLeft === 0) {
-  clearInterval(timer);
-  const finalScore = score;
-  const scores = JSON.parse(localStorage.getItem("scores")) || [];
-  scores.push({ score: finalScore });
-  localStorage.setItem("scores", JSON.stringify(scores)); // Store the score in local storage
-  window.location.href = "addscores.html"; // Goes to the score page after timer goes off
-}
-
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            const finalScore = score;
+            const scores = JSON.parse(localStorage.getItem("scores")) || [];
+            scores.push({ score: finalScore });
+            localStorage.setItem("scores", JSON.stringify(scores));
+          //  console.log(`Timer reached zero. Final Score: ${finalScore}`);
+            window.location.href = "addscores.html";
+        }
     }, 1000);
 }
 
@@ -36,6 +37,7 @@ document.getElementById("submit").addEventListener("click", function() {
     if (userAnswer === nums[0] + nums[1]) {
         score++;
         document.getElementById("score").textContent = `Score: ${score}`;
+       // console.log(`Correct answer! New Score: ${score}`);
     }
 
     document.getElementById("answer").value = "";
